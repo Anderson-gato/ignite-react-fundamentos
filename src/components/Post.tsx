25b4a163c15d2c3c1e8304eafeda99c198b1f55d
under-sin <andersonvieira818@yahoo.com.br>
2022-07-15 07:53:26 -0300

    3 files modified

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,74 +1,74 @@
-import { ChangeEvent, FormEvent, InvalidEvent, useState } from 'react'
+import { ChangeEvent, FormEvent, InvalidEvent, useState } from 'react';
 
-import { format, formatDistanceToNow } from 'date-fns'
-import ptBR from 'date-fns/locale/pt-BR'
+import { format, formatDistanceToNow } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 
-import { Avatar } from './Avatar'
-import { Comment } from './Comment'
+import { Avatar } from './Avatar';
+import { Comment } from './Comment';
 
-import styles from './Post.module.css'
+import styles from './Post.module.css';
 
 interface Author {
-  avatarUrl: string
-  name: string
-  role: string
+  avatarUrl: string;
+  name: string;
+  role: string;
 }
 
 interface Content {
-  type: 'paragraph' | 'link'
-  content: string
+  type: 'paragraph' | 'link';
+  content: string;
 }
 
 interface PostProps {
-  author: Author
-  publishedAt: Date
-  content: Content[]
+  author: Author;
+  publishedAt: Date;
+  content: Content[];
 }
 
 export function Post({ author, publishedAt, content }: PostProps) {
-  const [comments, setComments] = useState(['Post muito bacana'])
+  const [comments, setComments] = useState(['Post muito bacana']);
 
-  const [newCommentText, setNewCommentText] = useState('')
+  const [newCommentText, setNewCommentText] = useState('');
 
   const publishedDateFormatted = format(
     publishedAt,
     "dd 'de' LLLL 'às' HH:mm'h'",
     {
-      locale: ptBR
+      locale: ptBR,
     }
-  )
+  );
 
   const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
     locale: ptBR,
-    addSuffix: true
-  })
+    addSuffix: true,
+  });
 
   function handleCreateNewComment(event: FormEvent) {
-    event.preventDefault()
+    event.preventDefault();
 
-    setComments([...comments, newCommentText])
-    setNewCommentText('')
+    setComments([...comments, newCommentText]);
+    setNewCommentText('');
   }
 
   function handleNewCommentChange(event: ChangeEvent<HTMLTextAreaElement>) {
-    event.target.setCustomValidity('')
+    event.target.setCustomValidity('');
 
-    setNewCommentText(event.target.value)
+    setNewCommentText(event.target.value);
   }
 
   function handleNewCommentInvalid(event: InvalidEvent<HTMLTextAreaElement>) {
-    event.target.setCustomValidity('Esse campo é obrigatório!')
+    event.target.setCustomValidity('Esse campo é obrigatório!');
   }
 
   function deleteComment(commentToDelete: string) {
-    const newCommentWithoutDeleteOne = comments.filter(comment => {
-      return comment !== commentToDelete
-    })
+    const newCommentWithoutDeleteOne = comments.filter((comment) => {
+      return comment !== commentToDelete;
+    });
 
-    setComments(newCommentWithoutDeleteOne)
+    setComments(newCommentWithoutDeleteOne);
   }
 
-  const inNewCommentEmpty = newCommentText.length === 0
+  const inNewCommentEmpty = newCommentText.length === 0;
 
   return (
     <article className={styles.post}>
@@ -90,15 +90,15 @@ export function Post({ author, publishedAt, content }: PostProps) {
       </header>
 
       <div className={styles.content}>
-        {content.map(line => {
+        {content.map((line) => {
           if (line.type === 'paragraph') {
-            return <p key={line.content}>{line.content}</p>
+            return <p key={line.content}>{line.content}</p>;
           } else if (line.type === 'link') {
             return (
               <p key={line.content}>
                 <a href="#">{line.content}</a>
               </p>
-            )
+            );
           }
         })}
       </div>
@@ -123,16 +123,16 @@ export function Post({ author, publishedAt, content }: PostProps) {
       </form>
 
       <div className={styles.commentList}>
-        {comments.map(comment => {
+        {comments.map((comment) => {
           return (
             <Comment
               key={comment}
               content={comment}
               onDeleteComment={deleteComment}
             />
-          )
+          );
         })}
       </div>
     </article>
-  )
+  );
 }
